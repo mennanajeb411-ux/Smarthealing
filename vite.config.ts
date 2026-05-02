fa5477765c2f0e0294e44e734594ef679b968d15
Eng/Menna N <mennanajeb411@gmail.com>
2026-05-02 03:03:41 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -34,3 +34,6 @@ export default defineConfig({
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
+export default {
+  base:,
+};
